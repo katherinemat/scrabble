f180676657fa66373cf7654e18e90fee182d6b27
Kaz Matthews and Marc Larkin <kaz+marc@>
2017-02-16 09:55:12 -0800

    meet the criteria of all specs. Allows any word to receive a score

--- Objects/Scrabble.cs
+++ Objects/Scrabble.cs
@@ -9,27 +9,69 @@ namespace ScrabbleScore.Objects
     private char[] _input;
     private int _score;
     private char[] _one = {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'};
-    // private char[] _two = {'D', 'G'};
-    // private char[] _three = {'B', 'C', 'M', 'P'};
-    // private char[] _four = {'F', 'H', 'V', 'W', 'Y'};
-    // private char[] _five = {'K'};
-    // private char[] _eight = {'J', 'X'};
-    // private char[] _ten = {'Q', 'Z'};
+    private char[] _two = {'D', 'G'};
+    private char[] _three = {'B', 'C', 'M', 'P'};
+    private char[] _four = {'F', 'H', 'V', 'W', 'Y'};
+    private char[] _five = {'K'};
+    private char[] _eight = {'J', 'X'};
+    private char[] _ten = {'Q', 'Z'};
 
     public Scrabble (string input)
     {
       _input = input.ToUpper().ToCharArray();
-      Console.WriteLine(_input);
     }
 
     public int Score()
     {
-        for(int i = 0; i < _one.Length; i++)
+        for (int j = 0; j < _input.Length; j++)
         {
-            for (int j = 0; j < _input.Length; j++) {
+            for(int i = 0; i < _one.Length; i++)
+            {
                 if(_input[j] == _one[i])
                 {
                     _score += 1;
+                }
+            }
+            for(int o = 0; o < _two.Length; o++)
+            {
+                if(_input[j] == _two[o])
+                {
+                    _score += 2;
+                }
+            }
+            for(int p = 0; p < _three.Length; p++)
+            {
+                if(_input[j] == _three[p])
+                {
+                    _score += 3;
+                }
+            }
+            for(int q = 0; q < _four.Length; q++)
+            {
+                if(_input[j] == _four[q])
+                {
+                    _score += 4;
+                }
+            }
+            for(int g = 0; g < _five.Length; g++)
+            {
+                if(_input[j] == _five[g])
+                {
+                    _score += 5;
+                }
+            }
+            for(int k = 0; k < _eight.Length; k++)
+            {
+                if(_input[j] == _eight[k])
+                {
+                    _score += 8;
+                }
+            }
+            for(int h = 0; h < _ten.Length; h++)
+            {
+                if(_input[j] == _ten[h])
+                {
+                    _score += 10;
                 }
             }
         }
--- Tests/ScrabbleTest.cs
+++ Tests/ScrabbleTest.cs
@@ -12,26 +12,40 @@ namespace ScrabbleScore.Objects
     //   string userInput = "a";
     //   Scrabble newScrabble = new Scrabble(userInput);
     //   string userOutput = newScrabble.Score();
-    //   Console.WriteLine(userOutput);
+    /
     //   Assert.Equal("A", userOutput);
     // }
-    // [Fact]
-    // public void InputLetter_InputA_ReturnLetterValue()
-    // {
-    //   string userInput = "u";
-    //   Scrabble newScrabble = new Scrabble(userInput);
-    //   int userOutput = newScrabble.Score();
-    //   Console.WriteLine(userOutput);
-    //   Assert.Equal(1, userOutput);
-    // }
     [Fact]
-    public void InputMultipleLetters_CheckArrays_ReturnTotalScore()
+    public void InputLetter_InputA_ReturnLetterValue()
+    {
+      string userInput = "u";
+      Scrabble newScrabble = new Scrabble(userInput);
+      int userOutput = newScrabble.Score();
+      Assert.Equal(1, userOutput);
+    }
+    [Fact]
+    public void InputMultipleVowels_CheckArrays_ReturnTotalScore()
     {
       string userInput = "uae";
       Scrabble newScrabble = new Scrabble(userInput);
       int userOutput = newScrabble.Score();
-      Console.WriteLine(userOutput);
       Assert.Equal(3, userOutput);
+    }
+    [Fact]
+    public void InputMultipleLetters_CheckArrays_ReturnTotalScore()
+    {
+      string userInput = "doge";
+      Scrabble newScrabble = new Scrabble(userInput);
+      int userOutput = newScrabble.Score();
+      Assert.Equal(6, userOutput);
+    }
+    [Fact]
+    public void InputEntireWord_CheckArrays_ReturnTotalScore()
+    {
+      string userInput = "Supercalifragilisticexpialidocious";
+      Scrabble newScrabble = new Scrabble(userInput);
+      int userOutput = newScrabble.Score();
+      Assert.Equal(56, userOutput);
     }
   }
 }
